5a8456b9b547a5dfdc5ec833bc7440d040fe9a94
MarcosAniCury <curymarcos@live.com>
2023-11-30 00:31:52 -0300

    csv extract cells informations

---
+++ src/utils/csv_utils.py
@@ -0,0 +1,16 @@
+from src.singleton.cells_singleton import CellListSingleton, Cell
+import csv
+
+
+def read_csv(file_path):
+    cells = CellListSingleton()
+    with open(file_path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)
+        for line in csv_reader:
+            cells.append(Cell(*line))
+
+
+read_csv('../assets/classifications.csv')
+cells = CellListSingleton()
+print(cells.shared_list[0])
