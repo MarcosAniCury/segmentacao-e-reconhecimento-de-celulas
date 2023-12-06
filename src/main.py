3ef56d560ed4cb01934f66290b0831a794b96b0a
MarcosAniCury <curymarcos@live.com>
2023-12-06 00:56:11 -0300

    Add csv cells path

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from src.controller.image_classification import ImageClassification
-from src.controller.image_preprocessor import ImagePreProcessor
-from src.controller.image_descriptor import ImageDescriptor
-from src.controller.image_segment import ImageSegment
+from src.controller.image_classification_controller import ImageClassificationController
+from src.controller.image_preprocessor_controller import ImagePreProcessorController
+from src.controller.image_descriptor_controller import ImageDescriptorController
+from src.controller.image_segment_controller import ImageSegmentController
 from src.utils.csv_utils import read_csv
 from src.utils.os_utils import OSUtils
 import os
@@ -10,21 +10,22 @@ import os
 CSV_FILE_PATH = os.path.join(OSUtils.project_images_root, 'classifications.csv')
 # TODO: trocar esse variável pelo input
 INPUT_IMAGES_PATH = os.path.join(OSUtils.project_images_root, 'images')
+CSV_CELLS_PATH = os.path.join(OSUtils.project_images_root, 'cells_characteristics.csv')
 SEGMENTED_IMAGES_PATH = os.path.join(OSUtils.project_images_root, 'segmented_images')
 CROPPED_IMAGES_PATH = os.path.join(OSUtils.project_images_root, 'cropped_images')
 CLASSIFIED_IMAGES_PATH = os.path.join(OSUtils.project_images_root, 'classified_images')
 
-print("Segment Image")
-ImageSegment.segment_images(INPUT_IMAGES_PATH, SEGMENTED_IMAGES_PATH)
-
 print("Read CSV")
 read_csv(CSV_FILE_PATH)
 
+print("Segment Image")
+ImageSegmentController.segment_images(INPUT_IMAGES_PATH, SEGMENTED_IMAGES_PATH, CSV_CELLS_PATH)
+
 print("Crop Image")
-ImagePreProcessor.crop_image_per_cell(SEGMENTED_IMAGES_PATH, CROPPED_IMAGES_PATH)
+ImagePreProcessorController.crop_image_per_cell(SEGMENTED_IMAGES_PATH, CROPPED_IMAGES_PATH)
 
 print("Descript Image")
-ImageDescriptor.descript_images(CROPPED_IMAGES_PATH)
+ImageDescriptorController.descript_images(CROPPED_IMAGES_PATH)
 
 print("Classification Image")
-ImageClassification.classify_images(CROPPED_IMAGES_PATH, CLASSIFIED_IMAGES_PATH)
+ImageClassificationController.classify_images(CROPPED_IMAGES_PATH, CLASSIFIED_IMAGES_PATH)
