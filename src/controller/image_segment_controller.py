2a542db350d180fd49942cee1e8e029c8ffbb3ca
MarcosAniCury <curymarcos@live.com>
2023-12-06 00:57:46 -0300

    Add excel cell generate

--- src/controller/image_segment_controller.py
+++ src/controller/image_segment_controller.py
@@ -1,4 +1,6 @@
 from src.singleton.cells_singleton import CellListSingleton
+from src.controller.csv_controller import CSVController
+from src.utils.image_utils import ImageUtils
 from src.utils.os_utils import OSUtils
 from PIL import Image
 import numpy as np
@@ -6,18 +8,21 @@ import cv2
 import os
 
 
-class ImageSegment:
+class ImageSegmentController:
 
     @staticmethod
-    def segment_images(INPUT_IMAGES_PATH, SEGMENTED_IMAGES_PATH):
+    def segment_images(INPUT_IMAGES_PATH, SEGMENTED_IMAGES_PATH, CSV_CELLS_PATH):
         files_in_folder = OSUtils.get_files_in_folder(INPUT_IMAGES_PATH)
         OSUtils.mkdir(SEGMENTED_IMAGES_PATH)
+        csv_cells = CSVController()
+        csv_cells.open_csv(CSV_CELLS_PATH)
         for file in files_in_folder:
-            segmented_image = ImageSegment.segment_image(os.path.join(INPUT_IMAGES_PATH, file), file)
+            segmented_image = ImageSegmentController.segment_image(os.path.join(INPUT_IMAGES_PATH, file), file, csv_cells)
             segmented_image.save(os.path.join(SEGMENTED_IMAGES_PATH, file))
+        csv_cells.close_csv()
 
     @staticmethod
-    def segment_image(image_path, image_name):
+    def segment_image(image_path, image_name, csv_cells):
         cells = CellListSingleton()
 
         # Leitura da imagem
@@ -39,17 +44,23 @@ class ImageSegment:
         # Filtrar contornos com área semelhante à de um círculo
         filtered_contours = []
         for contour in contours:
-            area = cv2.contourArea(contour)
             perimeter = cv2.arcLength(contour, True)
 
             # Verificar se o perímetro é zero antes de calcular a circularidade
             if perimeter != 0:
+                area = cv2.contourArea(contour)
                 circularity = 4 * np.pi * area / (perimeter * perimeter)
 
                 # Ajuste o valor de circularity conforme necessário
                 if circularity > 0.5:
+                    compactness = area / (perimeter * perimeter)
+                    eccentricity = ImageUtils.calculate_eccentricity(contour)
+
                     filtered_contours.append(contour)
-                    cells.insert_centroid_in_cell(contour, image_name)
+                    cell = cells.insert_centroid_in_cell(contour, image_name)
+                    if cell:
+                        csv_cells_data = [str(cell.cell_id), str(area), str(compactness), str(eccentricity)]
+                        csv_cells.insert_row(csv_cells_data)
 
         # Criar máscara para os contornos filtrados
         cells_mask = np.zeros_like(gray)
