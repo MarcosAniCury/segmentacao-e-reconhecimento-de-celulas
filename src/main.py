c8ea2416bb14d18e1dfdf91366a616756dc0b2c2
MarcosAniCury <curymarcos@live.com>
2023-12-06 02:14:33 -0300

    Update main to implement scatterplot

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from src.controller.image_classification_controller import ImageClassificationCo
 from src.controller.image_preprocessor_controller import ImagePreProcessorController
 from src.controller.image_descriptor_controller import ImageDescriptorController
 from src.controller.image_segment_controller import ImageSegmentController
+from src.controller.scatterplot_controller import ScatterplotController
+from src.utils.scatterplot_utils import format_items_to_scatterplot_pattern
 from src.utils.csv_utils import read_csv
 from src.utils.os_utils import OSUtils
 import os
@@ -10,6 +12,7 @@ import os
 CSV_FILE_PATH = os.path.join(OSUtils.project_images_root, 'classifications.csv')
 # TODO: trocar esse variável pelo input
 INPUT_IMAGES_PATH = os.path.join(OSUtils.project_images_root, 'images')
+SCATTERPLOT_PATH = os.path.join(OSUtils.project_images_root, 'scatterplot.png')
 CSV_CELLS_PATH = os.path.join(OSUtils.project_images_root, 'cells_characteristics.csv')
 SEGMENTED_IMAGES_PATH = os.path.join(OSUtils.project_images_root, 'segmented_images')
 CROPPED_IMAGES_PATH = os.path.join(OSUtils.project_images_root, 'cropped_images')
@@ -20,6 +23,9 @@ read_csv(CSV_FILE_PATH)
 
 print("Segment Image")
 ImageSegmentController.segment_images(INPUT_IMAGES_PATH, SEGMENTED_IMAGES_PATH, CSV_CELLS_PATH)
+
+print("Create Scatterplot")
+ScatterplotController.generate_scatterplot(format_items_to_scatterplot_pattern(CSV_CELLS_PATH), SCATTERPLOT_PATH)
 
 print("Crop Image")
 ImagePreProcessorController.crop_image_per_cell(SEGMENTED_IMAGES_PATH, CROPPED_IMAGES_PATH)
